8e68497115986af4ed0b8f1c31c78c86290fbb0e
dawn <v_wangshengyan@xdf.cn>
2016-03-31 18:17:51 +0800

    添加了一点东西

---
+++ Apollo/app/src/main/java/com/dawn/apollo/apollo/http/OkHttpStack.java
@@ -0,0 +1,12 @@
+package com.dawn.apollo.apollo.http;
+
+import com.android.volley.toolbox.HurlStack;
+import com.squareup.okhttp.OkHttpClient;
+
+/**
+ * Created by dawn-pc on 2016/3/31.
+ */
+public class OkHttpStack extends HurlStack {
+
+//    private OkHttpClient
+}
