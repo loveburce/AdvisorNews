2ca197bc851cad8c498d5792f69c8c2a7e27f41b
dawn <v_wangshengyan@xdf.cn>
2016-04-07 17:50:09 +0800

    添加了一些类

---
+++ Apollo/app/src/main/java/com/dawn/apollo/apollo/fragment/BaseFragment.java
@@ -0,0 +1,7 @@
+package com.dawn.apollo.apollo.fragment;
+
+/**
+ * Created by dawn-pc on 2016/4/7.
+ */
+public class BaseFragment {
+}
---
+++ Apollo/app/src/main/java/com/dawn/apollo/apollo/fragment/NewsFragmentA.java
@@ -0,0 +1,33 @@
+package com.dawn.apollo.apollo.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.dawn.apollo.apollo.R;
+
+public class NewsFragmentA extends Fragment {
+	String text;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		Bundle args = getArguments();
+		text = args != null ? args.getString("text") : "";
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_news_a, null);
+		TextView item_textview = (TextView)view.findViewById(R.id.item_textview);
+		item_textview.setText(text);
+		return view;
+	}
+
+}
