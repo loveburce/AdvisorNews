45c92ade87e134f7398c509e2ec9eafd9fc5a7e1
dawn <v_wangshengyan@xdf.cn>
2016-04-20 18:46:42 +0800

    新修改

--- Apollo/app/src/main/java/com/dawn/apollo/apollo/fragment/NewsFragmentA.java
+++ Apollo/app/src/main/java/com/dawn/apollo/apollo/fragment/NewsFragmentA.java
@@ -6,14 +6,26 @@ import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.dawn.apollo.apollo.R;
 import com.dawn.apollo.apollo.adapter.NewFragmentAAdapter;
+import com.dawn.apollo.apollo.http.HttpClientRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class NewsFragmentA extends Fragment {
 	private LinearLayout ll_topBarLinearBack;
@@ -44,6 +56,59 @@ public class NewsFragmentA extends Fragment {
 
 
 		return view;
+	}
+
+	private void getNewsData(){
+		String httpurl = "";
+		HttpClientRequest httpClientRequest = HttpClientRequest.getInstance(getActivity());
+		StringRequest stringRequest = new StringRequest(Request.Method.POST,httpurl,
+				new Response.Listener<String>() {
+					@Override
+					public void onResponse(String response) {
+						Log.d("ffffffffffffff", "response -> " + response);
+
+						JSONObject jsonObject = null;
+						String results;
+						try {
+							jsonObject = new JSONObject(response);
+							results = jsonObject.getString("results");
+
+//							tunnelInfoList = JsonUtil.fromJson(results, new TypeToken<ArrayList<TunnelInfo>>() {
+//							}.getType());
+//							Log.d("ffffffffffffff", "response -> " + tunnelInfoList.size());
+
+//							InitView();
+
+
+						} catch (JSONException e) {
+							e.printStackTrace();
+						}
+					}
+				}, new Response.ErrorListener() {
+			@Override
+			public void onErrorResponse(VolleyError error) {
+				Log.e("ffffffffffffff", error.getMessage(), error);
+			}
+		}) {
+			@Override
+			protected Map<String, String> getParams() {
+				//在这里设置需要post的参数
+				Map<String,String> params = new HashMap<String, String>();
+
+//                String sign = null;
+//                try {
+//                    sign = URLEncoder.encode(MD5Tools.GetMD5Code(Constant.PublicKey), "UTF-8");
+//                } catch (UnsupportedEncodingException e) {
+//                    e.printStackTrace();
+//                }
+//                params.put("sign", sign);
+//                Log.e("ffffffffffffff", " sign : "+sign);
+				return params;
+			}
+		};
+
+		httpClientRequest.addRequest(stringRequest);
+
 	}
 
 }
